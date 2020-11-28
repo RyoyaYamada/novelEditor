ad9bbf7f379f74e723f273d8a5def740f2634e02
user01 <yuu1848@gmail.com>
2020-11-28 11:05:51 +0900

    [add] NovelEditor.fmxl EditaArea
    画面レイアウト追加、EditArea作成

---
+++ src/application/EditArea.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.scene.control.TextArea;
+
+public class EditArea extends TextArea {
+
+	public EditArea() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public EditArea(String text) {
+		super(text);
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
