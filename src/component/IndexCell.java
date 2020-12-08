16a612555c1c52267c54bb3e09e9265d800cdb6a
user01 <yuu1848@gmail.com>
2020-12-09 00:03:04 +0900

    [fix] NovelIndex selected css
    NovelIndex選択時の背景色がうまく反映されないことがあるので、明示的にcssを設定してみた

--- src/component/IndexCell.java
+++ src/component/IndexCell.java
@@ -1,16 +1,12 @@
 package component;
 
-import javafx.scene.control.TextField;
-import javafx.scene.control.TreeCell;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.cell.TextFieldTreeCell;
 import model.IndexItem;
 import model.Part;
 import model.Section;
 
-public class IndexCell extends TreeCell<String> {
+public class IndexCell extends TextFieldTreeCell<String> {
 
-	private TextField titleArea;
 
 	public IndexCell() {
 		super();
@@ -42,68 +38,68 @@ public class IndexCell extends TreeCell<String> {
 	}
 
 
-	@Override
-	public void startEdit() {
-		// TODO 自動生成されたメソッド・スタブ
-		super.startEdit();
-
-		if (titleArea == null) {
-			createTtitleArea();
-		} else {
-			titleArea.setText(getString());
-		}
-		setText(null);
-		setGraphic(titleArea);
-		titleArea.selectAll();
-	}
-
-
-	@Override
-	protected void updateItem(String item, boolean empty) {
-		super.updateItem(item, empty);
-
-		if (empty) {
-			// 何のデータも結びついていなければ、何も表示しない
-			setText(null);
-			setGraphic(null);
-		} else {
-			// 何らかのデータが結びついている
-			if (isEditing()) {
-				// 編集中に再描画された場合は、textFieldを表示
-				if (titleArea != null) {
-					titleArea.setText(getString());
-				}
-				setText(null);
-				setGraphic(titleArea);
-			} else {
-				// 編集中以外はlabelを表示
-				setText(getString());
-				setGraphic(getTreeItem().getGraphic());
-			}
-		}
-	}
-
-	@Override
-	public void cancelEdit() {
-		// TODO 自動生成されたメソッド・スタブ
-		super.cancelEdit();
-		setText(getItem());
-		setGraphic(getTreeItem().getGraphic());
-	}
-
-	private void createTtitleArea() {
-		titleArea = new TextField(getString());
-		titleArea.setOnKeyReleased((KeyEvent t) -> {
-			if (t.getCode() == KeyCode.ENTER) {
-				commitEdit(titleArea.getText());
-			} else if (t.getCode() == KeyCode.ESCAPE) {
-				cancelEdit();
-			}
-		});
-	}
-
-	private String getString() {
-		// getItem: TreeItemのvalueを返す
-        return getItem() == null ? "" : getItem();
-    }
+//	@Override
+//	public void startEdit() {
+//		// TODO 自動生成されたメソッド・スタブ
+//		super.startEdit();
+//
+//		if (titleArea == null) {
+//			createTtitleArea();
+//		} else {
+//			titleArea.setText(getString());
+//		}
+//		setText(null);
+//		setGraphic(titleArea);
+//		titleArea.selectAll();
+//	}
+//
+//
+//	@Override
+//	protected void updateItem(String item, boolean empty) {
+//		super.updateItem(item, empty);
+//
+//		if (empty) {
+//			// 何のデータも結びついていなければ、何も表示しない
+//			setText(null);
+//			setGraphic(null);
+//		} else {
+//			// 何らかのデータが結びついている
+//			if (isEditing()) {
+//				// 編集中に再描画された場合は、textFieldを表示
+//				if (titleArea != null) {
+//					titleArea.setText(getString());
+//				}
+//				setText(null);
+//				setGraphic(titleArea);
+//			} else {
+//				// 編集中以外はlabelを表示
+//				setText(getString());
+//				setGraphic(getTreeItem().getGraphic());
+//			}
+//		}
+//	}
+//
+//	@Override
+//	public void cancelEdit() {
+//		// TODO 自動生成されたメソッド・スタブ
+//		super.cancelEdit();
+//		setText(getItem());
+//		setGraphic(getTreeItem().getGraphic());
+//	}
+//
+//	private void createTtitleArea() {
+//		titleArea = new TextField(getString());
+//		titleArea.setOnKeyReleased((KeyEvent t) -> {
+//			if (t.getCode() == KeyCode.ENTER) {
+//				commitEdit(titleArea.getText());
+//			} else if (t.getCode() == KeyCode.ESCAPE) {
+//				cancelEdit();
+//			}
+//		});
+//	}
+//
+//	private String getString() {
+//		// getItem: TreeItemのvalueを返す
+//        return getItem() == null ? "" : getItem();
+//    }
 }
--- src/controller/IndexCellFactory.java
+++ src/controller/IndexCellFactory.java
@@ -32,7 +32,7 @@ public class IndexCellFactory implements Callback<TreeView<String>, TreeCell<Str
 	 *
 	 */
 	private static final String DROP_HINT_STYLE = "-fx-border-color: #eea82f; -fx-border-width: 0 0 2 0; -fx-padding: 3 3 1 3";
-	private static final String DROP_IN_STYLE = "-fx-background: powderblue";
+//	private static final String DROP_IN_STYLE = "-fx-background: powderblue";
 	private static final String DEFAULT_CELL_STYLE = "-fx-background: white";
 
 
@@ -67,9 +67,9 @@ public class IndexCellFactory implements Callback<TreeView<String>, TreeCell<Str
 		return cell;
 	}
 
-	private void dragDetect(MouseEvent event, IndexCell treeCell, NovelIndex novelIndex) {
+	private void dragDetect(MouseEvent event, IndexCell indexCell, NovelIndex novelIndex) {
 
-		draggedNode = treeCell.getIndexItem();
+		draggedNode = indexCell.getIndexItem();
 
 		if (draggedNode == null) {
 			return;
@@ -81,21 +81,21 @@ public class IndexCellFactory implements Callback<TreeView<String>, TreeCell<Str
 			return;
 		}
 
-		Dragboard dragboard = treeCell.startDragAndDrop(TransferMode.MOVE);
+		Dragboard dragboard = indexCell.startDragAndDrop(TransferMode.MOVE);
 
 		ClipboardContent content = new ClipboardContent();
 		content.put(JAVA_FORMAT, draggedNode.getValue());
 		dragboard.setContent(content);
-		dragboard.setDragView(treeCell.snapshot(null, null));
+		dragboard.setDragView(indexCell.snapshot(null, null));
 		event.consume();
 	}
 
-	private void dragOver(DragEvent event, IndexCell treeCell, NovelIndex novelIndex) {
+	private void dragOver(DragEvent event, IndexCell indexCell, NovelIndex novelIndex) {
 		if (!event.getDragboard().hasContent(JAVA_FORMAT)) {
 			return;
 		}
 
-		IndexItem dragOverNode = treeCell.getIndexItem();
+		IndexItem dragOverNode = indexCell.getIndexItem();
 
 		if (draggedNode == null || dragOverNode == null || dragOverNode == draggedNode) {
 			clearDropLocation();
@@ -103,22 +103,22 @@ public class IndexCellFactory implements Callback<TreeView<String>, TreeCell<Str
 		}
 
 		event.acceptTransferModes(TransferMode.MOVE);
-		if (!Objects.equals(dropZone, treeCell)) {
+		if (!Objects.equals(dropZone, indexCell)) {
 			clearDropLocation();
-			this.dropZone = treeCell;
+			this.dropZone = indexCell;
 			dropZone.setStyle(DROP_HINT_STYLE);
 		}
 
 	}
 
-	private void dragDrop(DragEvent event, IndexCell treeCell,  NovelIndex novelIndex) {
+	private void dragDrop(DragEvent event, IndexCell indexCell,  NovelIndex novelIndex) {
 		Dragboard dragboard = event.getDragboard();
 
 		if (!dragboard.hasContent(JAVA_FORMAT)) {
 			return;
 		}
 
-		IndexItem dropTo = treeCell.getIndexItem();
+		IndexItem dropTo = indexCell.getIndexItem();
 		IndexItem droppedItemParent = draggedNode.getParentIndex();
 
 		if (dropTo.isChildOf(draggedNode)) {
